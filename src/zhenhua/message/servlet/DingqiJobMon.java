f75dfe2c249788649854c44fa613b58c6fe2c669
shenkeyu <fengyangluoyue@163.com>
2017-04-03 12:25:03 +0800

    201704031224定时器实现

---
+++ src/zhenhua/message/servlet/DingqiJobMon.java
@@ -0,0 +1,14 @@
+package zhenhua.message.servlet;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class DingqiJobMon implements Job
+{
+    @Override
+    public void execute(JobExecutionContext jec) throws JobExecutionException 
+    {
+        System.out.println("hello world Month");
+    }
+}
